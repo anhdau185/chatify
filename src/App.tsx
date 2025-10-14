7ab766f02a68e8c791d37e66124deb7a0fcd64dd
Anh Dau <anhddv98@gmail.com>
2025-10-14 11:47:22 +0700

    set up and integrate tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-3xl font-bold">Hello World!</h1>
       <p>Welcome to my Electron application.</p>
     </>
   );
