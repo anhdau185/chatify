57b60ff9eba75abf9150ff39f8a8e1d98b051f3c
Anh Dau <anhddv98@gmail.com>
2025-10-15 16:07:48 +0700

    integrate react query to the app

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -1,29 +1,45 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
 import { HashRouter, Navigate, Route, Routes } from 'react-router';
 import ChatScreen from './chat/components';
 import LoginScreen from './login/components';
 import { useAuthStore } from './shared/store/authStore';
+
+const queryClient = new QueryClient();
+
+function Providers({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
 
 export default function App() {
   const { user } = useAuthStore();
   const isAuthenticated = user != null;
 
   return (
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/"
-          index
-          element={
-            isAuthenticated ? <Navigate to="/chat" replace /> : <LoginScreen />
-          }
-        />
-        <Route
-          path="/chat"
-          element={
-            isAuthenticated ? <ChatScreen /> : <Navigate to="/" replace />
-          }
-        />
-      </Routes>
-    </HashRouter>
+    <Providers>
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/"
+            index
+            element={
+              isAuthenticated ? (
+                <Navigate to="/chat" replace />
+              ) : (
+                <LoginScreen />
+              )
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              isAuthenticated ? <ChatScreen /> : <Navigate to="/" replace />
+            }
+          />
+        </Routes>
+      </HashRouter>
+    </Providers>
   );
 }
