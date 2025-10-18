6e323320713ce4fe0673cdc64890c4dd4d8815aa
Anh Dau <anhddv98@gmail.com>
2025-10-18 17:15:53 +0700

    check auth upon visiting ChatScreen

--- src/modules/messaging/components/ChatScreen.tsx
+++ src/modules/messaging/components/ChatScreen.tsx
@@ -1,7 +1,20 @@
+import { useAuthentication } from '@/modules/auth';
+import { Navigate } from 'react-router';
 import ChatSidebar from './ChatSidebar';
+import ChatSkeleton from './ChatSkeleton';
 import ConversationArea from './ConversationArea';
 
 export default function ChatScreen() {
+  const { isFetching, isAuthenticated } = useAuthentication();
+
+  if (isFetching) {
+    return <ChatSkeleton />; // TODO: implement actual chat skeleton
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <div className="flex h-screen bg-slate-50">
       <ChatSidebar />
