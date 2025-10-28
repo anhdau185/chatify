84f1216ba20f681618b318bd5af62780bfeb5f36
Anh Dau <anhddv98@gmail.com>
2025-10-28 14:41:09 +0700

    impl view a photo

--- src/modules/messaging/components/MessagePhotosGrid.tsx
+++ src/modules/messaging/components/MessagePhotosGrid.tsx
@@ -1,4 +1,7 @@
 import clsx from 'clsx';
+import { Fragment } from 'react';
+
+import { Dialog, DialogContent, DialogTrigger } from '@components/ui/dialog';
 
 export default function MessagePhotosGrid({
   imageURLs,
@@ -6,38 +9,48 @@ export default function MessagePhotosGrid({
   imageURLs: string[];
 }) {
   return (
-    <div
-      className={clsx([
-        'mb-2',
-        imageURLs.length === 1 && 'max-w-xs',
-        imageURLs.length === 2 && 'max-w-sm',
-        imageURLs.length === 3 && 'max-w-md',
-        imageURLs.length >= 4 && 'max-w-lg',
-      ])}
-    >
+    <Dialog>
       <div
         className={clsx([
-          'grid gap-1',
-          imageURLs.length === 1 && 'grid-cols-1',
-          imageURLs.length === 2 && 'grid-cols-2',
-          imageURLs.length === 3 && 'grid-cols-3',
-          imageURLs.length === 4 && 'grid-cols-2',
-          imageURLs.length >= 5 && 'grid-cols-3',
+          'mb-2',
+          imageURLs.length === 1 && 'max-w-xs',
+          imageURLs.length === 2 && 'max-w-sm',
+          imageURLs.length === 3 && 'max-w-md',
+          imageURLs.length >= 4 && 'max-w-lg',
         ])}
       >
-        {imageURLs.map((imgURL, idx) => (
-          <div
-            key={idx}
-            className="relative aspect-square overflow-hidden rounded-lg bg-slate-100"
-          >
-            <img
-              src={imgURL}
-              alt={`Image ${idx + 1}`}
-              className="h-full w-full cursor-pointer object-cover transition-opacity hover:opacity-90"
-            />
-          </div>
-        ))}
+        <div
+          className={clsx([
+            'grid gap-1',
+            imageURLs.length === 1 && 'grid-cols-1',
+            imageURLs.length === 2 && 'grid-cols-2',
+            imageURLs.length === 3 && 'grid-cols-3',
+            imageURLs.length === 4 && 'grid-cols-2',
+            imageURLs.length >= 5 && 'grid-cols-3',
+          ])}
+        >
+          {imageURLs.map((url, idx) => (
+            <Fragment key={`${url}-${idx}`}>
+              <DialogTrigger>
+                <div className="relative aspect-square overflow-hidden rounded-lg bg-slate-100">
+                  <img
+                    src={url}
+                    alt={`Image ${idx + 1}`}
+                    className="h-full w-full cursor-pointer object-cover transition-opacity hover:opacity-90"
+                  />
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <img
+                  src={url}
+                  alt={`Image ${idx + 1}`}
+                  className="h-full w-full"
+                />
+              </DialogContent>
+            </Fragment>
+          ))}
+        </div>
       </div>
-    </div>
+    </Dialog>
   );
 }
