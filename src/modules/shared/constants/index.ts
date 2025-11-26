9952eb7d75320311b143b65e5c03f1ae41b3ca43
Anh Dau <anhddv98@gmail.com>
2025-11-26 10:59:25 +0700

    add module declaration for images

--- src/modules/shared/constants/index.ts
+++ src/modules/shared/constants/index.ts
@@ -1,5 +1,5 @@
 const API_HOST = 'localhost:8080'; // development only
 const EXAMPLE_PHOTO_URL = 'https://randomuser.me/api/portraits/lego/1.jpg';
-const SELF_CHAT_ROOM_NAME = 'My Documents';
+const SELF_CHAT_ROOM_NAME = 'My Document';
 
 export { API_HOST, EXAMPLE_PHOTO_URL, SELF_CHAT_ROOM_NAME };
